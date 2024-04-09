a69c00aa9b29e2e829e7787ea560c921ca2472b7
Thais Jacob Lannes <tjacobla@ext.unicaja.es>
2024-04-09 17:11:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Word Generator
+
+"Word Generator" is a beginner-friendly mobile application built using Flutter. This app generates random words and allows users to favorite their preferred ones. Following the step-by-step instructions provided in the Flutter codelab, the project introduces key concepts of Flutter development, including UI design, state management, and basic interactivity. Users can enjoy a seamless experience of discovering and saving interesting words with just a tap, making it an ideal starting point for learning Flutter app development.
+
+## Features
+
+- Generates random words on demand.
+- Allows users to favorite words they like.
+- Provides a simple and intuitive user interface.
+- Easy to navigate and interact with.
+
+## Getting Started
+
+1. Ensure you have Flutter installed. If not, follow the [official installation guide](https://flutter.dev/docs/get-started/install)
+2. Clone this repository
+   ```bash
+   git clone https://github.com/your-user/word_generator.git
+   ```
+3. Navigate to the project directory
+   ```bash
+   cd word_generator
+   ```
+4. Run the app
+   ```bash
+   flutter run
+   ```
---
+++ lib/main.dart
@@ -0,0 +1,215 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp()); //Executa o app definido em MyApp
+}
+
+class MyApp extends StatelessWidget {
+  //Significa que extende widgets que sao elementos que servem como base para criaçao de apps do Flutter. O proprio app é um widget.
+  //MyApp configura o app - nomeia, define tema visual e o widget inicial.
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'My first app in Flutter by Thais Jacob',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  //Extende o ChangeNotifier -> Significa que pode emitir notificaçoes sobre suas proprias mudanças. Ou seja, se o par de palavras mudar, alguns widgets necessitarao saber disso.
+  //Define o estado do app, definindo os dados necessário para funcionar. Aqui, uma variável com o par de palavras aleatórias.
+  //Estado criado e fornecido a todo o app através de ChangeNotifierProvider em MyApp, assim todo widget no app tem acesso ao estado.
+
+  var current = WordPair.random();
+  //reatribui o widget current a um novo wordpair
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+    //notifyListeners é um metodo de ChangeNotifier que notifica a qlq elemento que esteja observando MyAppState
+  }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = FavoritesPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: constraints.maxWidth >= 600,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text('Favorites'),
+                  ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('I like this one!'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Nah, next'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!
+        .copyWith(color: theme.colorScheme.onPrimary);
+
+    return Card(
+      color: theme.colorScheme.primary,
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
+      ),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites words:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+  }
+}
